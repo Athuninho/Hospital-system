e31d59a4f43ec94d27dab4baa74d4aa6af14a737
Asumanii <athman7970@gmail.com>
2026-05-08 14:44:40 +0300

    ok

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '@hospital/prisma';
 import { HealthController } from './health.controller';
+import { AuthModule } from './auth/auth.module';
+import { JwtStrategy } from './auth/jwt.strategy';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [HealthController],
+  providers: [JwtStrategy],
 })
 export class AppModule {}
