599a2a2c2c33d04cd4ec742a998c9944d2e2844a
Asumanii <athman7970@gmail.com>
2026-05-11 12:01:48 +0300

    good

--- libs/prisma/src/prisma.service.ts
+++ libs/prisma/src/prisma.service.ts
@@ -4,13 +4,7 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
-    super({
-      // In Prisma 7, we pass the connection details here or use a config file
-      // For direct connection:
-      datasource: {
-        url: process.env.DATABASE_URL,
-      },
-    } as any); // Type cast if types are not perfectly synced yet
+    super();
   }
 
   async onModuleInit() {
